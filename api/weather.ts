c7aca415bb1a276bfea40912f0f02a1487b0066a
清水舞 <s.amandamy2161@gmail.com>
2025-10-07 00:34:03 +0900

    fix: api setting

--- api/weather.ts
+++ api/weather.ts
@@ -1,6 +1,17 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://shishimai1996.github.io/weatherApp/"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   const { lat, lon } = req.query;
   const apiKey = process.env.OPENWEATHER_API_KEY;
 
