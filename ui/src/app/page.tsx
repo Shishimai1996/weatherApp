921bd143c4ff4e09271a531b26d6284d0b82b4cb
清水舞 <s.amandamy2161@gmail.com>
2025-10-11 23:55:30 +0900

    fix: api setting6

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -8,7 +8,7 @@ const fetchWeather = async (lat: number, lon: number) => {
     console.error("API is not applied. pls check env file.");
     return null;
   }
-  const weatherUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/weather?lat=${lat}&lon=${lon}`;
+  const weatherUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}api/weather?lat=${lat}&lon=${lon}`;
   try {
     const response = await fetch(weatherUrl, { cache: "force-cache" });
     if (!response.ok) {
